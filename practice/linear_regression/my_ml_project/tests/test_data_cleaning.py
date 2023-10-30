2b3cef1ef2f2ea3c0f522af0b8fa146e50047f96
applefi87 <55140921+applefi87@users.noreply.github.com>
2023-10-30 16:11:07 +0800

    test_data_cleaning.py

---
+++ practice/linear_regression/my_ml_project/tests/test_data_cleaning.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+from src.data.data_cleaning import clean_data
+import json
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+config_path = os.path.join(project_dir, 'config.json')
+with open(config_path, "r") as f:
+    config = json.load(f)
+SIZE_COLUMN = config["SIZE_COLUMN"]
+WEIGHT_COLUMN = config["WEIGHT_COLUMN"]
+
+def test_clean_data():
+    # Sample raw data with known issues
+    raw_data = pd.DataFrame({
+        "x": [1, 2, 3, None, 5],
+        WEIGHT_COLUMN: [10, 20, None, 40, 50]
+    })
+    cleaned = clean_data(raw_data,[],["x",WEIGHT_COLUMN])
+    cleaned = cleaned.reset_index(drop=True)
+    # Expected cleaned data
+    expected = pd.DataFrame({
+        "x": [1, 2, 5],
+        WEIGHT_COLUMN: [10, 20, 50]
+    }, dtype='int32')
+
+    pd.testing.assert_frame_equal(cleaned, expected)
